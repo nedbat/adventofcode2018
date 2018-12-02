e2edee9a354538cbcc1c061b47bb73145b214135
Ned Batchelder <ned@nedbatchelder.com>
2018-12-02 06:37:13 -0500

    Day 2, part 2

--- day02.py
+++ day02.py
@@ -45,3 +45,46 @@ def test_checksum():
 if __name__ == "__main__":
     ans = checksum(puzzle_input())
     print(f"Part 1: the checksum is {ans}")
+
+# Part 2, brute force
+
+def equal_but_one(s1, s2):
+    """Are s1 and s2 the same except for one character?"""
+    # For our purposes, the lengths are always the same, don't check.
+    same = sum((c1 == c2) for c1, c2 in zip(s1, s2))
+    return same == len(s1) - 1
+
+@pytest.mark.parametrize("s1, s2, ret", [
+    ("abcde", "axcye", False),
+    ("fghij", "fguij", True),
+])
+def test_equal_but_one(s1, s2, ret):
+    assert equal_but_one(s1, s2) == ret
+
+def find_differ_by_one(ids):
+    """Return a pair of ids that differ by one character."""
+    for s1 in ids:
+        for s2 in ids:
+            if equal_but_one(s1, s2):
+                return {s1, s2}
+
+def common_chars(s1, s2):
+    """Return the string of only the common chars between s1 and s2."""
+    return "".join(c1 for c1, c2 in zip(s1, s2) if c1 == c2)
+
+def test_find_differ_by_one():
+    ids = [
+        "abcde",
+        "fghij",
+        "klmno",
+        "pqrst",
+        "fguij",
+        "axcye",
+        "wvxyz",
+        ]
+    assert find_differ_by_one(ids) == {"fguij", "fghij"}
+    assert common_chars(*find_differ_by_one(ids)) == "fgij"
+
+if __name__ == "__main__":
+    ans = common_chars(*find_differ_by_one(puzzle_input()))
+    print(f"Part 2: answer is {ans}")
