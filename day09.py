18255a968ef5048ced8863062769de5390add892
Ned Batchelder <ned@nedbatchelder.com>
2018-12-09 14:51:01 -0500

    Day 9, part 2

--- day09.py
+++ day09.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2018/day/9
-# Nudgy bookkeeping
+# Nudgy bookkeeping in lists, which is O(n**2)
+# Part 2: implement a doubly-linked circular buffer
 
 import itertools
 
@@ -67,3 +68,90 @@ if __name__ == "__main__":
     game = Game(*INPUT)
     game.play()
     print(f"Part 1: the winning elf's score is {game.high_score()}")
+    # Part 2 with this code would take hours...
+
+class CircleNode:
+    __slots__ = ('cw', 'ccw', 'value')
+
+    def __init__(self, value):
+        self.cw = None
+        self.ccw = None
+        self.value = value
+
+class Circle:
+    def __init__(self, value):
+        self.current = CircleNode(value)
+        self.current.cw = self.current
+        self.current.ccw = self.current
+
+    def move_cw(self, n):
+        for _ in range(n):
+            self.current = self.current.cw
+
+    def move_ccw(self, n):
+        for _ in range(n):
+            self.current = self.current.ccw
+
+    def insert(self, value):
+        # Insert after the current node.
+        node = CircleNode(value)
+        node.cw = self.current.cw
+        self.current.cw.ccw = node
+        node.ccw = self.current
+        self.current.cw = node
+        self.current = node
+
+    def delete(self):
+        self.current.cw.ccw = self.current.ccw
+        self.current.ccw.cw = self.current = self.current.cw
+
+    @property
+    def value(self):
+        return self.current.value
+
+
+class GameWithCircle:
+    def __init__(self, nplayers, last_marble):
+        self.nplayers = nplayers
+        self.scores = [0] * nplayers
+        self.last_marble = last_marble
+        self.circle = None
+        self.current = 0
+
+    def high_score(self):
+        return max(self.scores)
+
+    def play(self):
+        # Place the 0th marble
+        self.circle = Circle(0)
+        marbles = range(1, self.last_marble + 1)
+        player_turns = itertools.cycle(range(self.nplayers))
+        for marble, player in zip(marbles, player_turns):
+            if marble % 23:
+                # Not a multiple of 23: the usual case
+                self.circle.move_cw(1)
+                self.circle.insert(marble)
+            else:
+                # Special case
+                self.scores[player] += marble
+                self.circle.move_ccw(7)
+                self.scores[player] += self.circle.value
+                self.circle.delete()
+
+@pytest.mark.parametrize("nplayers, last_marble, high_score", [
+    (9, 25, 32),
+    (10, 1618, 8317),
+    (13, 7999, 146373),
+    (17, 1104, 2764),
+    (21, 6111, 54718),
+    (30, 5807, 37305),
+])
+def test_game(nplayers, last_marble, high_score):
+    game = GameWithCircle(nplayers, last_marble)
+    game.play()
+    assert game.high_score() == high_score
+
+if __name__ == "__main__":
+    game = GameWithCircle(INPUT[0], INPUT[1] * 100)
+    game.play()
+    print(f"Part 2: the winning elf's score is {game.high_score()}")
