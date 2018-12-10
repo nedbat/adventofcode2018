cb47e96d2efa2ab6015a9336c321e710297ca98c
Ned Batchelder <ned@nedbatchelder.com>
2018-12-10 07:02:43 -0500

    Day 10, part 1

---
+++ day10.py
@@ -0,0 +1,66 @@
+# https://adventofcode.com/2018/day/10
+
+from dataclasses import dataclass
+import re
+
+@dataclass
+class Point:
+    x: int
+    y: int
+    dx: int
+    dy: int
+
+    def move(self):
+        return self.__class__(self.x+self.dx, self.y+self.dy, self.dx, self.dy)
+
+    @classmethod
+    def from_line(cls, line):
+        return cls(*map(int, re.findall(r"-?\d+", line)))
+
+class Sky:
+    def __init__(self, points):
+        self.points = list(points)
+
+    def tick(self):
+        return self.__class__(p.move() for p in self.points)
+
+    def bounds(self):
+        minx = min(p.x for p in self.points)
+        maxx = max(p.x for p in self.points)
+        miny = min(p.y for p in self.points)
+        maxy = max(p.y for p in self.points)
+        return minx, maxx, miny, maxy
+
+    def area(self):
+        minx, maxx, miny, maxy = self.bounds()
+        return (maxx - minx + 1) * (maxy - miny + 1)
+
+    def print(self):
+        minx, maxx, miny, maxy = self.bounds()
+        for y in range(miny, maxy+1):
+            for x in range(minx, maxx+1):
+                if any((p.x, p.y) == (x, y) for p in self.points):
+                    char = "#"
+                else:
+                    char = "."
+                print(char, end="")
+            print()
+
+def the_input(fname):
+    with open(fname) as f:
+        return f.read().splitlines()
+
+def find_minimal_area(sky):
+    last_sky = sky
+    while True:
+        sky = last_sky.tick()
+        if sky.area() > last_sky.area():
+            return last_sky
+        last_sky = sky
+
+test_sky = Sky(Point.from_line(l) for l in the_input("day10_test_input.txt"))
+find_minimal_area(test_sky).print()
+
+print()
+sky = Sky(Point.from_line(l) for l in the_input("day10_input.txt"))
+find_minimal_area(sky).print()
