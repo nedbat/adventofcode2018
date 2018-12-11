ed2e7a865217511aea1dca06fd3ee9ce010a1e35
Ned Batchelder <ned@nedbatchelder.com>
2018-12-11 06:13:19 -0500

    Day 11, part 2

--- day11.py
+++ day11.py
@@ -45,5 +45,42 @@ def test_best_square(serial_number, x, y, power):
 INPUT = 5535
 
 if __name__ == "__main__":
-    power, x, y = best_square(Grid(INPUT))
+    power, x, y = Grid(INPUT).best_square()
     print(f"Part 1: the fuel cell with the largest total power is at {(x,y)}")
+
+
+class Grid2(Grid):
+    def __init__(self, serial_number):
+        super().__init__(serial_number)
+
+        self.total = [[0] * 301 for _ in range(301)]
+
+        for x, y in range2d(1, 300, 1, 300):
+            self.total[x][y] = self.total[x][y-1] + self.total[x-1][y] - self.total[x-1][y-1] + self.power_level(x, y)
+
+    def square_total_power(self, x, y, size):
+        x -= 1
+        y -= 1
+        return self.total[x+size][y+size] - self.total[x][y+size] - self.total[x+size][y] + self.total[x][y]
+
+    def squares(self):
+        for x, y in range2d(1, 300, 1, 300):
+            large = max(x, y)
+            for size in range(1, 301-large+1):
+                power = self.square_total_power(x, y, size)
+                yield (power, x, y, size)
+
+    def best_square(self):
+        return max(self.squares())
+
+@pytest.mark.parametrize("serial_number, x, y, size, power", [
+    (18, 90, 269, 16, 113),
+    (42, 232, 251, 12, 119),
+])
+def test_grid2(serial_number, x, y, size, power):
+    assert Grid2(serial_number).best_square() == (power, x, y, size)
+
+
+if __name__ == "__main__":
+    power, x, y, size = Grid2(INPUT).best_square()
+    print(f"Part 2: the square with the largest total power is {(x,y,size)}")
