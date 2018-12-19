c2ccdaf074833f9d03f72de101706f83bd5649ab
Ned Batchelder <ned@nedbatchelder.com>
2018-12-19 07:09:46 -0500

    Part 2, doesn't work. Some disassembly to try to understand the program

--- day19.py
+++ day19.py
@@ -3,11 +3,12 @@
 from day16 import (
     addr, addi, mulr, muli, banr, bani, borr, bori,
     setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr,
+    A, B, C,
     )
 
 INPUT = """\
 #ip 2
-addi 2 16 2
+addi 2 16 2     # 0: rjump to 17
 seti 1 1 5
 seti 1 1 3
 mulr 5 3 4
@@ -18,13 +19,13 @@ addr 5 0 0
 addi 3 1 3
 gtrr 3 1 4
 addr 2 4 2
-seti 2 8 2
+seti 2 8 2      # jump to 3
 addi 5 1 5
 gtrr 5 1 4
 addr 4 2 2
-seti 1 5 2
+seti 1 5 2      # jump to 2
 mulr 2 2 2
-addi 1 2 1
+addi 1 2 1      # 17:
 mulr 1 1 1
 mulr 2 1 1
 muli 1 11 1
@@ -33,7 +34,7 @@ mulr 4 2 4
 addi 4 7 4
 addr 1 4 1
 addr 2 0 2
-seti 0 4 2
+seti 0 4 2      # jump to 1
 setr 2 8 4
 mulr 4 2 4
 addr 2 4 4
@@ -42,7 +43,7 @@ muli 4 14 4
 mulr 4 2 4
 addr 1 4 1
 seti 0 5 0
-seti 0 8 2
+seti 0 8 2      # jump to 1
 """.splitlines()
 
 TEST_INPUT = """\
@@ -67,7 +68,7 @@ class Device:
         self.ipreg = int(lines[0].split()[-1])
         self.prog = []
         for line in lines[1:]:
-            parts = line.split()
+            parts = line.split()[:4]
             opfunc = globals()[parts[0]]
             inst = (opfunc,) + tuple(map(int, parts[1:]))
             self.prog.append(inst)
@@ -91,6 +92,38 @@ class Device:
             if not (0 <= self.ip < len(self.prog)):
                 break
 
+    def disassemble(self):
+        ipreg = self.ipreg
+        for loc, inst in enumerate(self.prog):
+            inst = list(inst)
+            op = inst[0]
+            opname = op.__name__
+            operands = " ".join(map(str, inst[1:]))
+            comment = ""
+            if op == addi:
+                if inst[A] == ipreg and inst[C] == ipreg:
+                    dest = loc + inst[B] + 1
+                    comment = f"jump to {dest}"
+                elif inst[A] == inst[C]:
+                    comment = f"r{inst[C]} += {inst[B]}"
+                else:
+                    comment = f"r{inst[C]} = r{inst[A]} + {inst[B]}"
+            elif op == addr:
+                if inst[B] == inst[C]:
+                    inst[B], inst[A] = inst[A], inst[B]
+                if inst[A] == inst[C]:
+                    comment = f"r{inst[C]} += r{inst[B]}"
+                else:
+                    comment = f"r{inst[C]} = r{inst[A]} + r{inst[B]}"
+            elif op == seti:
+                if inst[C] == ipreg:
+                    dest = inst[A] + 1
+                    comment = f"jump to {dest}"
+                else:
+                    comment = f"r{inst[C]} = {inst[A]}"
+            comch = "#" if comment else ""
+            print(f"{loc:2d}: {opname} {operands:20} {comch} {comment}".rstrip())
+
 def test_device():
     device = Device()
     device.read_program(TEST_INPUT)
@@ -98,8 +131,23 @@ def test_device():
     assert device.regs[5] == 9
     assert device.ip == 7
 
-if __name__ == "__main__":
+def part1():
     device = Device()
     device.read_program(INPUT)
     device.run()
     print(f"Part 1: register 0 is {device.regs[0]}")
+
+def naive_part2():
+    device = Device()
+    device.read_program(INPUT)
+    device.regs[0] = 1
+    device.run()
+    print(f"Part 2: register 0 is {device.regs[0]}")
+
+def disassemble():
+    device = Device()
+    device.read_program(INPUT)
+    device.disassemble()
+
+if __name__ == "__main__":
+    disassemble()
